3960d8ea00fe262e6599c1b710d55e08db25fc0b
k.mikhaltsov <kmihaltsov@gmail.com>
2017-12-18 19:46:24 +0300

    Base Commit

--- dist/Autolinker.js
+++ dist/Autolinker.js
@@ -3394,22 +3394,19 @@ Autolinker.matcher.Hashtag = Autolinker.Util.extend( Autolinker.matcher.Matcher,
 Autolinker.matcher.Phone = Autolinker.Util.extend( Autolinker.matcher.Matcher, {
 
 	/**
-	 * The regular expression to match Phone numbers. Example match:
-	 *
-	 *     (123) 456-7890
+	 * The regular expression to match Phone numbers.
 	 *
 	 * This regular expression has the following capturing groups:
 	 *
-	 * 1. The prefixed '+' sign, if there is one.
+	 * 1. helper symbols to properly divide phone number from the rest of the text. group is used to calculate index offset
+	 * 2. phone
+	 * 3. The prefixed '+' sign, if there is one.
 	 *
 	 * @private
 	 * @property {RegExp} matcherRegex
 	 */
-    matcherRegex : /(?:(\+)?\d{1,3}[-\040.]?)?\(?\d{3}\)?[-\040.]?\d{3}[-\040.]?\d{4}([,;]*[0-9]+#?)*/g,    
+    matcherRegex : /(\s|^|[^\d][,.])(\(?(\+)?(?:[\/\-() ]?\d){4,}(?:\))?)(?=[,.][^\d]|\.$|\s|$)/g,
     
-    // ex: (123) 456-7890, 123 456 7890, 123-456-7890, +18004441234,,;,10226420346#, 
-    // +1 (800) 444 1234, 10226420346#, 1-800-444-1234,1022,64,20346#
-
 	/**
 	 * @inheritdoc
 	 */
@@ -3421,26 +3418,22 @@ Autolinker.matcher.Phone = Autolinker.Util.extend( Autolinker.matcher.Matcher, {
 
 		while ((match = matcherRegex.exec(text)) !== null) {
 			// Remove non-numeric values from phone number string
-			var matchedText = match[0],
-				cleanNumber = matchedText.replace(/[^0-9,;#]/g, ''), // strip out non-digit characters exclude comma semicolon and #
-				plusSign = !!match[1]; // match[ 1 ] is the prefixed plus sign, if there is one
-			if (this.testMatch(match[2]) && this.testMatch(matchedText)) {
-    			matches.push(new Autolinker.match.Phone({
-    				tagBuilder: tagBuilder,
-    				matchedText: matchedText,
-    				offset: match.index,
-    				number: cleanNumber,
-    				plusSign: plusSign
-    			}));
-            }
+			var indexOffset = match[1] ? match[1].length : 0,
+				matchedText = match[2],
+				cleanNumber = matchedText.replace(/[^0-9]/g, ''), // strip out non-digit characters
+				plusSign = !!match[3]; // match[ 1 ] is the prefixed plus sign, if there is one
+
+			matches.push(new Autolinker.match.Phone({
+				tagBuilder: tagBuilder,
+				matchedText: matchedText,
+				offset: match.index + indexOffset,
+				number: cleanNumber,
+				plusSign: plusSign
+			}));
 		}
 
 		return matches;
 	},
-
-	testMatch: function(text) {
-		return /\D/.test(text);
-	}
 
 } );
 
